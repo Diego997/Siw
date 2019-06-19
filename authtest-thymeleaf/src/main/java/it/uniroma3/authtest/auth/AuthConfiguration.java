97564f08d5854434f3bfe45d4015725b2e1b1533
alejandro <alexandro.wood@hotmail.com>
2019-06-19 10:45:32 +0200

    committopull

--- authtest-thymeleaf/src/main/java/it/uniroma3/authtest/auth/AuthConfiguration.java
+++ authtest-thymeleaf/src/main/java/it/uniroma3/authtest/auth/AuthConfiguration.java
@@ -51,7 +51,7 @@ public class AuthConfiguration extends WebSecurityConfigurerAdapter {
                     // only admin can access the admin page
                     .antMatchers(HttpMethod.GET, "/admin").hasAnyAuthority("ADMIN")
 
-                    // all authenticated users can access all the other pages (that is, welcome)
+          // all authenticated users can access all the other pages (that is, welcome)
                     .anyRequest().authenticated()
 
                 // login paragraph: we are going to define here how to login
