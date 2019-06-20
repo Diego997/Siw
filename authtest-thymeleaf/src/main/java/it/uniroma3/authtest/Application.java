f5c06ca55e6ff14631cf2167735c5d68458519f5
Valerio Marini <marinivalerio97@gmail.com>
2019-06-20 13:04:03 +0200

    Fixed the search on search.html

--- authtest-thymeleaf/src/main/java/it/uniroma3/authtest/Application.java
+++ authtest-thymeleaf/src/main/java/it/uniroma3/authtest/Application.java
@@ -2,11 +2,16 @@ package it.uniroma3.authtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
