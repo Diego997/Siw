9ff204f5af0a10f949b774a4369a936d7f8ec3a2
Valerio Marini <marinivalerio97@gmail.com>
2019-06-20 14:34:57 +0200

    Implemented the search for fotografia, fotografo and album in search.html.

--- authtest-thymeleaf/src/main/java/it/uniroma3/authtest/controller/SearchController.java
+++ authtest-thymeleaf/src/main/java/it/uniroma3/authtest/controller/SearchController.java
@@ -11,10 +11,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import it.uniroma3.authtest.model.Album;
 import it.uniroma3.authtest.model.Fotografia;
+import it.uniroma3.authtest.model.Fotografo;
 import it.uniroma3.authtest.service.FotografiaService;
 import it.uniroma3.authtest.service.FunzionarioService;
 import it.uniroma3.authtest.service.HibernateSearchService;
@@ -38,18 +41,50 @@ public class SearchController {
 
 
 	@RequestMapping("/search")
-	public String search(@RequestParam(value = "search", required = false) String q, Model model) {
+	public String search(@RequestParam(value = "search", required = false) String q, @RequestParam(value="tipo", required=false) String radio, Model model) {
 
-		List<Fotografia> searchResult = null;
-		try {
-			searchResult = searchService.fuzzySearchFotografia(q);
-		} catch (Exception ex) {
-			// here you should handle unexpected errors
-			// ...
-			// throw ex;
-		}	
 
-		model.addAttribute("search", searchResult);
+		if(radio!=null) {
+			if(radio.equals("fotografia")) {
+				try {
+					List<Fotografia> searchResult = null;
+					System.out.print(radio);
+					searchResult = searchService.fuzzySearchFotografia(q);
+					model.addAttribute("fotografie", searchResult);
+				} catch (Exception ex) {
+					// here you should handle unexpected errors
+					// ...
+					// throw ex;
+				}	
+			}
+
+			if(radio.equals("fotografo")) {
+				try {
+					List<Fotografo> searchResult = null;
+					System.out.print(radio);
+					searchResult = searchService.fuzzySearchFotografo(q);
+					model.addAttribute("fotografi", searchResult);
+				} catch (Exception ex) {
+					// here you should handle unexpected errors
+					// ...
+					// throw ex;
+				}	
+			}
+
+			if(radio.equals("album")) {
+				try {
+					List<Album> searchResult = null;
+					System.out.print(radio);
+					searchResult = searchService.fuzzySearchAlbum(q);
+					model.addAttribute("albums", searchResult);
+				} catch (Exception ex) {
+					// here you should handle unexpected errors
+					// ...
+					// throw ex;
+				}	
+			}	
+		}
+
 		return "search";
 
 
